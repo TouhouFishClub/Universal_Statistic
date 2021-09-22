087d664b76c1a2f42136301f6a3de16c38579444
ter <a26214311@gmail.com>
2021-09-22 13:16:40 +0800

    f

--- server/w.js
+++ server/w.js
@@ -33,7 +33,7 @@ app.get('/get_waiting_by_date',function(req,res){
 
 app.get('/list',function(req,res){
   var url = 'https://gw.app.universalbeijingresort.com/attraction/list?sort_type=0&support_express=0&suitable_children=0&accessibility=0&page=1&page_size=1000'
-  var url2 = 'http://54.238.79.53:6660/url?url='+encodeURIComponent(url);
+  var url2 = 'http://54.250.49.236:6660/url?url='+encodeURIComponent(url);
   request({
     url: url2,
     method: "GET"
