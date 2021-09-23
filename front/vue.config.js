2ca904df1cf1cf672dd06611f6ce0d1554b984e6
Flandre <flantan233@gmail.com>
2021-09-23 17:14:01 +0800

    unpack cdn

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,7 +1,42 @@
+const isProduction = process.env.NODE_ENV === 'production'
+const path = require('path')
+
+const patterns = [
+  path.join(__dirname, 'node_modules/vuetify/dist/vuetify.css'),
+  path.join(__dirname, 'node_modules/@mdi/font/css/materialdesignicons.min.css'),
+]
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  configureWebpack: config => {
+    if (isProduction) {
+      config.externals = {
+        'vue': 'Vue',
+        'vue-router': 'VueRouter',
+        'vuex': 'Vuex',
+        'axios': 'axios',
+        'vuetify': 'Vuetify',
+        'chart.js': 'chart.js',
+        'vue-chartjs': 'vue-chartjs',
+      }
+    }
+  },
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'css',
+      patterns: isProduction ? [] : patterns
+    }
+  },
+  chainWebpack: config => {
+    // 分析
+    if (process.env.use_analyzer) {
+      config
+        .plugin('webpack-bundle-analyzer')
+        .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+    }
+  },
   devServer: {
     proxy: 'http://flanb.msharebox.com:6660/'
   },
