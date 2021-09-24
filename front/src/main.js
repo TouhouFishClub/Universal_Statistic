d86fe7f0f42ba451f62fd2622a86644273969300
Flandre <flantan233@gmail.com>
2021-09-24 14:53:01 +0800

    change cdn

--- front/src/main.js
+++ front/src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import Toast from '@/components/global/Toast/index'
 
 Vue.config.productionTip = false
